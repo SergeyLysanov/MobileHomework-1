9aa64101b3ea0c9e63c12af76798c42155f42b39
SergeyLysanov <sergey@ubuntu.(none)>
2013-10-22 14:56:36 +0400

    Изменен стиль.

--- src/com/example/mobilehomework_1/MainActivity.java
+++ src/com/example/mobilehomework_1/MainActivity.java
@@ -12,30 +12,30 @@ import android.widget.EditText;
 import com.example.mobilehomework_1.EditDialogFragment;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class MainActivity extends FragmentActivity 
-				implements EditDialogFragment.EditDialogListener
-				,	CheckDialogFragment.CheckDialogListener 
-{
+public class MainActivity extends FragmentActivity implements
+		EditDialogFragment.EditDialogListener,
+		CheckDialogFragment.CheckDialogListener {
 	static final String STATE_CHECK = "check_box";
 	private boolean isChecked = false;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_container);
-		
-	    // Check whether we're recreating a previously destroyed instance
-	    if (savedInstanceState != null) {
-	        // Restore value of members from saved state
-	        this.isChecked = savedInstanceState.getBoolean(STATE_CHECK);
-	    	return;
-	    } 
-	    
-	    // Create a new Fragment to be placed in the activity layout
-        MainFragment mainFragment = new MainFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.fragment_container, mainFragment);
-        transaction.commit();
+
+		// Check whether we're recreating a previously destroyed instance
+		if (savedInstanceState != null) {
+			// Restore value of members from saved state
+			this.isChecked = savedInstanceState.getBoolean(STATE_CHECK);
+			return;
+		}
+
+		// Create a new Fragment to be placed in the activity layout
+		MainFragment mainFragment = new MainFragment();
+		FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
+		transaction.add(R.id.fragment_container, mainFragment);
+		transaction.commit();
 	}
 
 	@Override
@@ -44,81 +44,76 @@ public class MainActivity extends FragmentActivity
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	@Override
-	public void onSaveInstanceState(Bundle savedInstanceState) 
-	{
-	    savedInstanceState.putBoolean(STATE_CHECK, isChecked);
-	    super.onSaveInstanceState(savedInstanceState);
-	}
-	
-	//Button listener
-	public void showDialog(View view)
-	{
-        // Create an instance of the dialog fragment and show it
-        EditDialogFragment dialog = new EditDialogFragment();
-        dialog.show(getSupportFragmentManager(), "EditDialogFragment");
-	}
-	
-	//Button listener
-    public void showCheckDialog(View view)
-    {
-        if(!isChecked)
-        {
-        	CheckDialogFragment dialog = new CheckDialogFragment();
-        	dialog.show(getSupportFragmentManager(), "CheckDialogFragment");
-        }
-    }
 
-	//Button listener
-	public void showScreen2(View view)
-	{
-		EditText mainEdit  = (EditText)findViewById(R.id.mainEditText);
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+		savedInstanceState.putBoolean(STATE_CHECK, isChecked);
+		super.onSaveInstanceState(savedInstanceState);
+	}
+
+	// Button listener
+	public void showDialog(View view) {
+		// Create an instance of the dialog fragment and show it
+		EditDialogFragment dialog = new EditDialogFragment();
+		dialog.show(getSupportFragmentManager(), "EditDialogFragment");
+	}
+
+	// Button listener
+	public void showCheckDialog(View view) {
+		if (!isChecked) {
+			CheckDialogFragment dialog = new CheckDialogFragment();
+			dialog.show(getSupportFragmentManager(), "CheckDialogFragment");
+		}
+	}
+
+	// Button listener
+	public void showScreen2(View view) {
+		EditText mainEdit = (EditText) findViewById(R.id.mainEditText);
 		String sText = mainEdit.getText().toString();
 		SecondFragment fragment = SecondFragment.newInstance(sText);
-		
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+		FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
 		transaction.replace(R.id.fragment_container, fragment);
 		transaction.addToBackStack(null);
 		transaction.commit();
 	}
-	
-	//Button listener
-	public void showScreen3(View view)
-	{
+
+	// Button listener
+	public void showScreen3(View view) {
 		ThirdFragment fragment = new ThirdFragment();
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+		FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
 		transaction.replace(R.id.fragment_container, fragment);
 		transaction.addToBackStack(null);
 		transaction.commit();
 	}
-	
-	//Button listener
-	public void showScreen4(View view)
-	{
+
+	// Button listener
+	public void showScreen4(View view) {
 		FourthFragment fragment = new FourthFragment();
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+		FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
 		transaction.replace(R.id.fragment_container, fragment, "fourth_screen");
 		transaction.addToBackStack(null);
 		transaction.commit();
 	}
-    
+
 	@Override
 	public void onDialogPositiveClick(String sText) {
-		EditText mainEdit  = (EditText)findViewById(R.id.mainEditText);
+		EditText mainEdit = (EditText) findViewById(R.id.mainEditText);
 		mainEdit.setText(sText);
 	}
 
 	@Override
 	public void onDialogNegativeClick() {
 		System.out.println("onDialogNegativeClick");
-		
+
 	}
-	
-	//Check dialog
+
+	// Check dialog
 	@Override
-	public void onDialogPositiveClick(boolean isChecked) 
-	{
+	public void onDialogPositiveClick(boolean isChecked) {
 		this.isChecked = isChecked;
 	}
 }
